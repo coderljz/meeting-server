0d3d5b80c6031650e625bdf6bb46eadbf3a966bb
coderljz <1499404629@qq.com>
2020-12-04 19:04:54 +0800

    完成：spring项目启动

---
+++ src/main/java/com/ljz/meetingserver/MeetingServerApplication.java
@@ -0,0 +1,13 @@
+package com.ljz.meetingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ljz/meetingserver/MeetingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljz.meetingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
